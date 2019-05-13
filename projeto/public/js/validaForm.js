cc2cacdc43e6f6bbf76bc278cea780306091192f
vinicius <viniciussouzav@gmail.com>
2019-05-13 17:40:20 -0300

    Atualização que o lumbriga me pediu na aula !

--- projeto/public/js/validaForm.js
+++ projeto/public/js/validaForm.js
@@ -5,6 +5,13 @@ $(document).ready(() =>{
     });
 });
 
+function limpaCampos() {
+    $('#codigo').val('');
+    $('#nome').val('');
+    $('#email').val('');
+    $('#cursos').val('');
+}
+
 function validar() {
     let codigo = $('#codigo').val();
     if(codigo == '' || codigo <= 0){
@@ -20,6 +27,7 @@ function validar() {
     } else
         $('#nome').removeClass('erro');
 
+    let curso = $('#cursos').val();
     if($('#cursos').val() == ''){
         $('#cursos').addClass('erro');
         return;
@@ -27,19 +35,28 @@ function validar() {
         $('#cursos').removeClass('erro');
 
 
-    addTable(codigo, nome);
+    addTable(codigo, nome, curso);
+    limpaCampos();
 }
 
-function addTable(c, n) {
+function addTable(c, n, cu) {
     $('tbody').append(`
-        <tr onclick="removeTable(this)">
+        <tr id="${c}#${n}#${cu}">
             <td>${c}</td>
             <td>${n}</td>
-            <td><a href="#">X</a></td>
+            <td onclick="removeTable(this)"><a href="#">X</a></td>
+            <td onclick="alt(this)"><a href="#">alt</a></td>
         </tr>
     `)
 }
 
 function removeTable(tr) {
-    $(tr).remove();
+    $(tr.closest('tr')).remove();
+}
+
+function alt(tr) {
+    let data = tr.closest('tr').id.split('#');
+    $('#codigo').val(data[0]);
+    $('#nome').val(data[1]);
+    $('#cursos').val(data[2]);
 }
